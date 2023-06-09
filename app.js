2a085c92aa0fe5114f7ea30ef79cccfec1af775b
jaoviitor <joaoviiitorvieira@gmail.com>
2023-06-09 20:42:35 -0300

    add access control

--- app.js
+++ app.js
@@ -11,6 +11,17 @@ app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+app.use((req, res, next) =>{
+    res.header('Access-Control-Allow-Origin', '*'); // só permite o acesso da API por esse servidor
+    res.header('Access-Control-Allow-Header', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+
+    if(req.method === 'OPTIONS'){
+        res.header('Acess-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+        return res.status(200).send({});
+    }
+    next();
+})
+
 app.use('/usuario', rotaUsuario);
 
 //TRATAMENTO PARA QUANDO NÃO ENCONTRA UMA ROTA
